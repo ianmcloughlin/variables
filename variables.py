51d965e6d41a1608071ec07b3ad4f25e9eca1723
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2020-01-27 10:55:49 +0000

    Done

--- variables.py
+++ variables.py
@@ -6,15 +6,22 @@ y = 2
 
 z = x + y
 
-x = 914
-y = 3
-
-z = x + y
-
 print(z)
 
 fac7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
 
-print(fac)
+print(fac7)
 
-print = 45
+b = True
+c = False
+
+print(c)
+
+s = "ian.mcloughlin@gmit.ie"
+
+print(s[0:14:2])
+
+l = ['a', 'b', 'c', 4, 5, 6, 7, 8, 9]
+
+print(l[2:6:3])
+
